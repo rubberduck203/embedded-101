956ced193fff9afc3dcf10872552ba81c99ec2fe
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2019-04-24 21:56:59 -0400

    Add usb filters for Arduino brand boards
    https://forum.arduino.cc/index.php?topic=446788.0

--- mac-setup-docker.sh
+++ mac-setup-docker.sh
@@ -5,3 +5,5 @@ docker-machine stop
 vboxmanage modifyvm default --usbehci on
 docker-machine start
 vboxmanage usbfilter add 0 --target default --name ftdi --vendorid 0x0403 --productid 0x6015
+vboxmanage usbfilter add 0 --target default --name arduino-org --vendorid 0x2A03
+vboxmanage usbfilter add 0 --target default --name arduino-cc --vendorid 0x2341
